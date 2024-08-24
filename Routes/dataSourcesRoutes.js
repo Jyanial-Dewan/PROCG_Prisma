95e7979821a4e59e40c5dd4b274e39dec202eebd
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-08-24 12:12:15 +0600

    datasource routes and controller added

---
+++ Routes/dataSourcesRoutes.js
@@ -0,0 +1,12 @@
+const Router = require("express");
+const dataSourcesController = require("../Controller/dataSourcesController");
+
+const router = Router();
+
+router.get("/", dataSourcesController.getDataSources);
+router.get("/:id", dataSourcesController.getUniqueDataSource);
+router.post("/", dataSourcesController.createDataSource);
+router.delete("/:id", dataSourcesController.deleteDataSource);
+router.put("/:id", dataSourcesController.updateDataSource);
+
+module.exports = router;
