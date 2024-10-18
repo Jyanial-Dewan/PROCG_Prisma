3d78c02f0a4b22bfc07302b08cf094f2d3644961
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-10-18 23:37:45 +0600

    re-organized code and add access entitlement element

---
+++ Routes/accessEntitlementElementsRoutes.js
@@ -0,0 +1,23 @@
+const Router = require("express");
+const accessEntitlementElementsController = require("../Controller/accessEntitlementElementsController");
+
+const router = Router();
+
+router.get(
+  "/",
+  accessEntitlementElementsController.getAccessEntitlementElement
+);
+router.get(
+  "/:id",
+  accessEntitlementElementsController.getUniqueAccessEntitlementElement
+);
+router.post(
+  "/",
+  accessEntitlementElementsController.createAccessEntitlementElement
+);
+router.delete(
+  "/",
+  accessEntitlementElementsController.deleteAccessEntitlementElement
+);
+
+module.exports = router;
