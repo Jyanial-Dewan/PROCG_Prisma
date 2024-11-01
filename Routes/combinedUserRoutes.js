e7a9fc9eb812717b71ee4c83b247419908d81f81
Jyanial Dewan <djyanial@gmail.com>
2024-11-01 23:43:53 +0600

    wrapper added for combined user

--- Routes/combinedUserRoutes.js
+++ Routes/combinedUserRoutes.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.post("/", combinedUrserController.createCombinedUser);
 router.get("/:page/:limit", combinedUrserController.getCombinedUsers);
+router.get("/v2", combinedUrserController.getFlaskCombinedUser);
+router.post("/v2", combinedUrserController.createFlaskCombinedUser);
 
 module.exports = router;
