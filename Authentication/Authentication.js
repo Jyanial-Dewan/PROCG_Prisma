10d7c71359ff218614a3463bf3eaf8e27d066e2c
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-12-24 23:07:26 +0600

    token generate functionality done

--- Authentication/Authentication.js
+++ Authentication/Authentication.js
@@ -6,16 +6,19 @@ dotenv.config();
 
 const JWT_SECRET_ACCESS_TOKEN = process.env.JWT_SECRET_ACCESS_TOKEN;
 const JWT_SECRET_REFRESH_TOKEN = process.env.JWT_SECRET_REFRESH_TOKEN;
+
+//Generate access token and refresh token
 const generateAccessTokenAndRefreshToken = (props) => {
   const accessToken = jwt.sign(props, JWT_SECRET_ACCESS_TOKEN, {
-    expiresIn: "1m",
+    expiresIn: "1h",
   });
   const refreshToken = jwt.sign(props, JWT_SECRET_REFRESH_TOKEN, {
-    expiresIn: "33m",
+    expiresIn: "7d",
   });
   return { accessToken, refreshToken };
 };
 
+// Login
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -51,7 +54,7 @@ exports.login = async (req, res) => {
       },
     });
     if (!user) {
-      res.status(404).json({ error: "User not found." });
+      res.status(404).json({ message: "User not found." });
     } else {
       const userCredential = await prisma.def_user_credentials.findUnique({
         where: {
@@ -81,12 +84,10 @@ exports.login = async (req, res) => {
           .cookie("refresh_token", refreshToken, {
             httpOnly: true,
             secure: true,
-            // maxAge: 20 * 60 * 1000, // 7 days
           })
           .cookie("access_token", accessToken, {
             httpOnly: true,
             secure: false,
-            // maxAge: 15 * 1000, // 15 minutes
           })
           .json({
             isLoggedIn: true,
@@ -99,7 +100,7 @@ exports.login = async (req, res) => {
             issuedAt: new Date(),
           });
       } else {
-        return res.status(401).json({ error: "Invalid credential" });
+        return res.status(401).json({ message: "Invalid credential" });
       }
     }
   } catch (error) {
@@ -107,13 +108,14 @@ exports.login = async (req, res) => {
   }
 };
 
+// Logout
 exports.logout = (req, res) => {
   try {
     return res
       .status(200)
       .clearCookie("access_token")
       .clearCookie("refresh_token")
-      .json("Logged out successfully");
+      .json({ message: "Logged out successfully" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
@@ -134,53 +136,75 @@ exports.user = (req, res) => {
 
 // refresh token
 exports.refreshToken = async (req, res) => {
-  const refreshTokenValue = req.cookies.refresh_token || req.body.refresh_token;
+  const refreshTokenValue =
+    req.cookies?.refresh_token ||
+    req.body?.refresh_token ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
   if (!refreshTokenValue) {
-    return res.status(401).json({ error: "No refresh token provided" });
+    return res.status(401).json({ message: "No refresh token provided" });
   }
   try {
-    const decoded = jwt.verify(refreshTokenValue, JWT_SECRET_REFRESH_TOKEN);
-    const user = await prisma.def_users.findUnique({
-      where: {
-        user_id: decoded?.user_id,
-      },
-    });
-    if (!user) {
-      return res
-        .status(401)
-        .json({ error: "Invalid or expired refresh token" });
-    }
-    // Generate new access token and refresh token
-    const { accessToken, refreshToken } = generateAccessTokenAndRefreshToken({
-      isLoggedIn: true,
-      user_id: user.user_id,
-      user_type: user.user_type,
-      user_name: user.user_name,
-      tenant_id: user.tenant_id,
-      issuedAt: new Date(),
-    });
+    jwt.verify(
+      refreshTokenValue,
+      JWT_SECRET_REFRESH_TOKEN,
+      async (err, user) => {
+        if (err) {
+          //if token expired
+          if (err.name === "TokenExpiredError") {
+            return res
+              .status(401)
+              .clearCookie("access_token")
+              .clearCookie("refresh_token")
+              .json({ message: "Unauthorized Access: Token has expired" });
+          }
+          //if token is invalid
+          return res.status(403).json({ message: "Forbidden: Invalid token" });
+        }
+        const response = await prisma.def_users.findUnique({
+          where: {
+            user_id: user?.user_id,
+          },
+        });
+        if (!response) {
+          return res
+            .status(401)
+            .json({ message: "Invalid or expired refresh token" });
+        }
+        // Generate new access token and refresh token
+        const { accessToken, refreshToken } =
+          generateAccessTokenAndRefreshToken({
+            isLoggedIn: true,
+            user_id: user.user_id,
+            user_type: user.user_type,
+            user_name: user.user_name,
+            tenant_id: user.tenant_id,
+            issuedAt: new Date(),
+          });
 
-    return res
-      .status(200)
-      .cookie("refresh_token", refreshToken, {
-        httpOnly: true,
-        secure: true,
-      })
-      .cookie("access_token", accessToken, {
-        httpOnly: true,
-        secure: false,
-      })
-      .json({
-        isLoggedIn: true,
-        user_id: user.user_id,
-        user_type: user.user_type,
-        user_name: user.user_name,
-        tenant_id: user.tenant_id,
-        access_token: accessToken,
-        refresh_token: refreshToken,
-        issuedAt: new Date(),
-      });
+        return res
+          .status(200)
+          .cookie("refresh_token", refreshToken, {
+            httpOnly: true,
+            secure: true,
+          })
+          .cookie("access_token", accessToken, {
+            httpOnly: true,
+            secure: false,
+          })
+          .json({
+            isLoggedIn: true,
+            user_id: user.user_id,
+            user_type: user.user_type,
+            user_name: user.user_name,
+            tenant_id: user.tenant_id,
+            access_token: accessToken,
+            refresh_token: refreshToken,
+            issuedAt: new Date(),
+          });
+      }
+    );
   } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired refresh token" });
+    return res.status(500).json({ error: "Invalid or expired refresh token" });
   }
 };
--- Authentication/verifyUser.js
+++ Authentication/verifyUser.js
@@ -4,6 +4,7 @@ const verifyUser = (req, res, next) => {
   try {
     const token =
       req?.cookies?.access_token ||
+      req?.body?.access_token ||
       req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token)
@@ -13,11 +14,13 @@ const verifyUser = (req, res, next) => {
 
     jwt.verify(token, process.env.JWT_SECRET_ACCESS_TOKEN, (err, user) => {
       if (err) {
+        //if token expired
         if (err.name === "TokenExpiredError") {
           return res
             .status(401)
             .json({ message: "Unauthorized Access: Token has expired" });
         }
+        //if token is invalid
         return res.status(403).json({ message: "Forbidden: Invalid token" });
       }
 
