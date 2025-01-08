d537a504e3f90dfbb36561f1516a5a8dc89e9879
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-01-08 11:30:08 +0600

    add user ip (cause incognito mode not fetch ip)

--- Routes/authenticationRoutes.js
+++ Routes/authenticationRoutes.js
@@ -7,5 +7,6 @@ router.post("/", authentication.login);
 router.get("/", authentication.logout);
 router.get("/user", authentication.user);
 router.get("/refresh-token", authentication.refreshToken);
+router.get("/get-user-ip-info", authentication.userIpInfo);
 
 module.exports = router;
