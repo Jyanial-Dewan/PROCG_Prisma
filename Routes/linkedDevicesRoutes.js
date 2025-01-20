4d8bab9cd1dbdd687c46da1c5f64c5afe64ed0d8
nepoionchakma <nepolionchakma.nc@gmail.com>
2025-01-20 12:11:23 +0600

    change inactive device method

--- Routes/linkedDevicesRoutes.js
+++ Routes/linkedDevicesRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/:user_id", linkedDevicesController.getDevices);
 router.post("/add-device", linkedDevicesController.addDevice);
-router.post(
+router.put(
   "/inactive-device/:user_id/:id",
   linkedDevicesController.inactiveDevice
 );
