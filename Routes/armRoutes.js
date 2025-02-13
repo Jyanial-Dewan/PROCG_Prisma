bff40e8f6fc778109edd9c1bc94c03949e7dc479
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-02-13 17:30:22 +0600

    Manage Execution Methods api added

--- Routes/armRoutes.js
+++ Routes/armRoutes.js
@@ -2,6 +2,12 @@ const Router = require("express");
 const armControllers = require("../Controller/armController");
 
 const router = Router();
+// Execution Methods
+router.get("/show-execution-methods", armControllers.showExecutionMethods); 
+router.get("/show-execution-methods/:page/:limit",  armControllers.showExecutionMethodsLazyLoading); 
+router.post("/create-execution-method", armControllers.createExecutionMethod); 
+router.put("/update-execution-method/:internal_execution_method", armControllers.updateExecutionMethod); 
+
 // Register/Edit Asynchronous Tasks
 router.get("/show-tasks", armControllers.getARMTasks);
 router.get("/show-tasks/:page/:limit", armControllers.getARMTasksLazyLoading);
@@ -11,16 +17,12 @@ router.put("/edit-task/:task_name", armControllers.editARMTask);
 router.put("/cancel-task/:task_name", armControllers.cancelARMTask);
 
 // Task Params
-router.get("/:task_name", armControllers.getUserTaskNameParams);
-router.get("/:task_name/:page/:limit", armControllers.getTaskParamsLazyLoading);
+router.get("/task-params/:task_name", armControllers.getUserTaskNameParams);
+router.get("/task-params/:task_name/:page/:limit", armControllers.getTaskParamsLazyLoading);
 router.post("/add-task-params/:task_name", armControllers.addTaskParams);
-router.put(
-  "/update-task-params/:task_name/:arm_param_id",
-  armControllers.updateTaskParams
-);
-router.delete(
-  "/delete-task-params/:task_name/:arm_param_id",
-  armControllers.deleteTaskParams
-);
+router.put("/update-task-params/:task_name/:arm_param_id",armControllers.updateTaskParams);
+router.delete("/delete-task-params/:task_name/:arm_param_id",armControllers.deleteTaskParams);
+
+ 
 
 module.exports = router;
